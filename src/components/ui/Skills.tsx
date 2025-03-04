e923ea22b34c517ae8309084eba42503be4744fc
msetu1 <msetu5763@gmail.com>
2025-03-04 23:45:51 +0600

    code add

--- src/components/ui/Skills.tsx
+++ src/components/ui/Skills.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { TbBrandTypescript } from "react-icons/tb";
+
 const Skills = () => {
     return (
         <div className='flex lg:max-w-[80%] lg:mx-auto my-20 '>
@@ -21,12 +21,41 @@ const Skills = () => {
     </div>
             </div>
             <div className="w-[50%] border">
-            <h2 className='text-[30px] font-bold mb-5 underline'>Skills </h2>
+            <h2 className='text-[30px] font-bold mb-5 underline text-center'>Skills </h2>
              <div className="grid grid-cols-1 lg:grid-cols-4 gap-3">
-                <div className="flex gap-2 border border-[#15803d]">
-<TbBrandTypescript className="text-3xl text-[#15803d]"/>
+                <button className="gap-2 border-2 border-[#15803d] rounded-full px-4 py-2">
                 TypeScript
-                </div>
+                </button>
+                <button className="gap-2 border-2 border-[#15803d] rounded-full px-4 py-2">
+                JavaScript
+                </button>
+                <button className="gap-2 border-2 border-[#15803d] rounded-full px-4 py-2">
+                Node.js
+                </button>
+                <button className="gap-2 border-2 border-[#15803d] rounded-full px-4 py-2">
+                Express.js
+                </button>
+                <button className="gap-2 border-2 border-[#15803d] rounded-full px-4 py-2">
+                MongoDB
+                </button>
+                <button className="gap-2 border-2 border-[#15803d] rounded-full px-4 py-2">
+                Mongoose
+                </button>
+                <button className="gap-2 border-2 border-[#15803d] rounded-full px-4 py-2">
+                DBMS
+                </button>
+                <button className="gap-2 border-2 border-[#15803d] rounded-full px-4 py-2">
+                SQL
+                </button>
+                <button className="gap-2 border-2 border-[#15803d] rounded-full px-4 py-2">
+                React.js
+                </button>
+                <button className="gap-2 border-2 border-[#15803d] rounded-full px-4 py-2">
+                Redux
+                </button>
+                <button className="gap-2 border-2 border-[#15803d] rounded-full px-4 py-2">
+                Next.js
+                </button>
              </div>
             </div>
         </div>
